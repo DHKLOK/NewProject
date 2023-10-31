81d978f629cd81e224f52711c262c3da764a04b9
DHKLOKlaptop <kwrifles@gmail.com>
2023-10-31 11:45:10 -0400

    installed mongodb packages

---
+++ dbconfig.js
@@ -0,0 +1,16 @@
+// dbConfig.js
+
+const mongoose = require('mongoose');
+
+const dbUrl = 'mongodb-3lrc:27017';
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+module.exports = mongoose;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Import the MongoDB connection
+const mongoose = require('./dbConfig');
+
+// Define a Mongoose schema and model if needed
+const Schema = mongoose.Schema;
+const YourSchema = new Schema({
+  // Define your schema fields here
+});
+
+const YourModel = mongoose.model('YourModel', YourSchema);
+
+// Example: Create a new document
+const newDoc = new YourModel({ /* data here */ });
+newDoc.save((err) => {
+  if (err) throw err;
+  console.log('Document saved successfully');
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
