20cd8cb9f37a7748963236333ddb8da66c184bff
DHKLOKlaptop <kwrifles@gmail.com>
2023-11-02 12:59:41 -0400

    Reupload

--- vite-project/dbconfig.js
+++ vite-project/dbconfig.js
@@ -1,10 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-  const collection = client.db("mongodb").collection("mycollection");
-  // perform actions on the collection object
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error("Error connecting to MongoDB:", err);
+    return;
+  }
+
+  const db = client.db(); // Get the default database
+  const collection = db.collection("mycollection");
+  
+  // Perform actions on the collection object
+  
+  // Close the MongoDB connection when done
   client.close();
 });
