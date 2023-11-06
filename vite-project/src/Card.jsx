9059b54541a1751dbe5b23e118c6d4b70125a535
DHKLOKlaptop <kwrifles@gmail.com>
2023-11-06 12:06:04 -0500

    Relocated old project for deployment

---
+++ vite-project/src/Card.jsx
@@ -0,0 +1,14 @@
+function Card(){
+
+return(
+    <div className="card">
+    <img src="vite-project\src\simon.jpg"></img>
+    <h2>Duhak</h2>
+    <p>I am a amateur programmer and WoW addicted gamer</p>
+
+    </div>
+);
+
+}
+
+export default Card
