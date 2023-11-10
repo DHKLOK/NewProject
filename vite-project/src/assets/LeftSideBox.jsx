8e5d4bc78284030363bb4b68386ce8a2ccaf601b
DHKLOKlaptop <kwrifles@gmail.com>
2023-11-10 17:00:20 -0500

    Sidebar will now collaspe

--- vite-project/src/assets/LeftSideBox.jsx
+++ vite-project/src/assets/LeftSideBox.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '/src/Card.jsx';
 import '/src/index.css';
+import './LeftBox.css';
 
 function LeftBox() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="secondLayer">
-        <Card></Card>
-      </div>
+    <div className={`secondLayer ${isCollapsed ? 'collapsed' : ''}`}>
+      <button onClick={toggleSidebar}>
+        {isCollapsed ? 'Expand Sidebar' : 'Collapse Sidebar'}
+      </button>
+
+      {!isCollapsed && <Card />}
+    </div>
   );
 }
-   
- export default LeftBox;
+
+export default LeftBox;
