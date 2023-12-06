f0b96a54ddf462b8a70be15845711c4a453d79fb
DHKLOKlaptop <kwrifles@gmail.com>
2023-12-06 12:01:38 -0500

    push

--- vite-project/src/ColorContext.jsx
+++ vite-project/src/ColorContext.jsx
@@ -1,17 +1,18 @@
-// ColorContext.js
 import React, { createContext, useContext, useState } from 'react';
 
 const ColorContext = createContext();
 
 export const ColorProvider = ({ children }) => {
   const [commonBackgroundColor, setCommonBackgroundColor] = useState('rgb(243, 130, 130)');
+  const [commonTextColor, setCommonTextColor] = useState('red');
 
   const handleCommonColorChange = (color) => {
     setCommonBackgroundColor(color);
+    setCommonTextColor(color);
   };
 
   return (
-    <ColorContext.Provider value={{ commonBackgroundColor, handleCommonColorChange }}>
+    <ColorContext.Provider value={{ commonBackgroundColor, commonTextColor, handleCommonColorChange }}>
       {children}
     </ColorContext.Provider>
   );
--- vite-project/src/timer.jsx
+++ vite-project/src/timer.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import Background from './Background';
+import { useColor } from './ColorContext';
 
-function TimerDisplay({BackGround}) {
+function TimerDisplay() {
+  const { commonTextColor } = useColor();
   const [currentTime, setCurrentTime] = useState(new Date());
 
-  // Use useEffect to update the time and set up the interval
   useEffect(() => {
     const displayTimer = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
 
-    // Clean up the interval when the component unmounts
     return () => clearInterval(displayTimer);
   }, []); // Empty dependency array ensures useEffect runs once after the initial render
 
   return (
-    <div className='timer-container'>
+
+    <div className='timer-container' style={{ color: commonTextColor, textShadow: '1px 1px 1px rgba(255, 255, 255, 0.8)', fontFamily: 'Roboto', top:130 }}>
       <p>Current Time: {currentTime.toLocaleTimeString()}</p>
     </div>
   );
